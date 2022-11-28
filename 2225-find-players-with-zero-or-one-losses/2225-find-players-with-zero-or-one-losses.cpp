3a2d20e44311923e6b509a37b7c12b4047aa4afa
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2022-11-28 09:07:55 +0530

    Time: 781 ms (88.98%), Space: 169.6 MB (64.55%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -7,7 +7,6 @@ public:
             win[m[i][0]]++; los[m[i][1]]++;
         }
         for(auto xx:win){
-            // cout<<xx.first<<" "<<xx.second<<endl;
             if(los.find(xx.first) == los.end()){
                 vp[0].push_back(xx.first);
             }
@@ -22,16 +21,6 @@ public:
         }
         sort(vp[0].begin(),vp[0].end());
         sort(vp[1].begin(),vp[1].end());
-        // for(int i=0; i<vp.size(); i++){cout<<endl;
-        //     for(int j=0; j<vp[i].size(); j++){
-        //         cout<<vp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        // cout<<endl;if(vp[i].size() == 0){
-        // for(auto xx:los){
-        //     cout<<xx.first<<" "<<xx.second<<endl;
-        // }
         return vp;
     }
 };
