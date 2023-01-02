6854cf3ddb3804ae74ecadc7f564ccfc20d9c00c
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-01-02 16:58:20 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (24.57%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        int small = 0,cptl = 0;
-        for(int i=0; i<word.size(); i++){
+        int small = 0,cptl = 0; int n = word.size();
+        for(int i=0; i<n; i++){
             (word[i]-'0' >= 17 && word[i]-'0'<=42)?cptl++:small++;
         }
-        if(small==word.size() || (small==word.size()-1 && (word[0]-'0' >= 17 && word[0]-'0'<=42)) || cptl==word.size()){
-            return true;
-        }
-        return false;
+        return (small==n || (small==n-1 && (word[0]-'0' >= 17 && word[0]-'0'<=42)) || cptl==n)?true:false;
     }
 };
