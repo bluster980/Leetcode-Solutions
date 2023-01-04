9c4d79b27c2155ac7bdbdd1ce6f84dcaea8d517c
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-01-04 18:29:43 +0530

    Time: 12 ms (17.07%), Space: 12.1 MB (75.14%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int temp = INT_MIN;
+        for(int i=nums.size()-1; i>0; i--){
+            if(nums[i] > nums[i-1]){
+                temp = i-1; break;
+            }
+        }
+        // cout<<temp<<endl;
+        if(temp!=INT_MIN){
+        for(int i=nums.size()-1; i>=0; i--){
+            if(nums[i] > nums[temp]){
+                swap(nums[i],nums[temp]); break;
+            }
+        }
+        reverse(nums.begin()+temp+1,nums.end());}
+        else{
+            reverse(nums.begin(),nums.end());
+        }
     }
 };
