3d261697c4c08c6ff5ae2afb4358a1bf57aee853
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-01-07 18:15:10 +0530

    Time: 14 ms (88.39%), Space: 13.3 MB (23.15%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& m) {
         int row=m[0].size(),col=m.size();
-        vector<pair<int,int>> vp; vector<int> visr(row),visc(col);
+        vector<pair<int,int>> vp; vector<int> visc(row,0),visr(col,0);
         for(int i=0; i<col; i++){
             for(int j=0; j<row; j++){
                 if(m[i][j] == 0){
@@ -10,22 +10,19 @@ public:
                 }
             }
         }
-        // for(int i=0; i<vp.size(); i++){
-            // cout<<vp[i].first<<" "<<vp[i].second<<endl;
-        // }
-        // cout<<"#####"<<endl;
         for(int i=0; i<vp.size(); i++){
-            for(int j=0; j<row; j++){
-                // cout<<vp[i].first<<" "<<j<<endl;
-                // cout<<m[vp[i].first][j]<<" ";
-                m[vp[i].first][j] = 0;
+            if(visr[vp[i].first] == 0)
+            {
+                for(int j=0; j<row; j++){
+                    m[vp[i].first][j] = 0; visr[vp[i].first] = 1;
+                }
             }
-            for(int j=0; j<col; j++){
-                m[j][vp[i].second] = 0;
-                 // cout<<j<<" "<<vp[i].second<<endl;
+            if(visc[vp[i].second] == 0){
+                for(int j=0; j<col; j++){
+                    m[j][vp[i].second] = 0; visc[vp[i].second] = 1;
+                }
             }
-            // cout<<endl;
+            
         }
-        // cout<<endl;
     }
 };
