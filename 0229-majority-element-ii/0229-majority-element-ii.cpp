4bbdd113ef45ee240dcc33ea705b3860d993cc80
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-01-11 09:03:57 +0530

    Time: 15 ms (78.44%), Space: 16 MB (56.25%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> ans;
-        int curr1=INT_MIN,curr2=INT_MIN,cnt1=0,cnt2=0;
+        map<int,int> mp;
         for(int i=0; i<nums.size(); i++){
-            if(nums[i]==curr1) cnt1++;
-            else if(nums[i]==curr2) cnt2++;
-            else if(cnt1 == 0) curr1=nums[i],cnt1++;
-            else if(cnt2 == 0) curr2=nums[i],cnt2++;
-            else {cnt1--; cnt2--;}
+            mp[nums[i]]++;
         }
-        int count1=0,count2=0;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]==curr1) count1++;
-            else if(nums[i]==curr2) count2++;
+        vector<int> v;
+        for(auto xx:mp){
+            if(xx.second > floor(nums.size()/3.0)){
+                v.push_back(xx.first);
+            }
         }
-        if(count1 > nums.size()/3) ans.push_back(curr1);
-        if(count2 > nums.size()/3) ans.push_back(curr2);
-        
-        return ans;
+        return v;
     }
 };
