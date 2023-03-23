5f59d4fa33181ab3d6802dbfb989038803998c12
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-03-23 23:44:30 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (51.75%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     string largestGoodInteger(string num) {
-        bool flag = false;
-        string temp; temp.push_back(num[0]); temp.push_back(num[1]); temp.push_back(num[2]); int maxx = 0; 
-        if(temp[0]==temp[1] && temp[1]==temp[2]) maxx = max(maxx,stoi(temp));
-        if(temp[0]==temp[1] && temp[1]==temp[2] && temp[0]=='0') flag = true;
-        for(int i=3; i<num.size(); i++){
-            temp.erase(temp.begin()); temp.push_back(num[i]);
-            if(temp[0]==temp[1] && temp[1]==temp[2]) maxx = max(maxx,stoi(temp));
-            if(temp[0]==temp[1] && temp[1]==temp[2] && temp[0]=='0') flag = true;
+        int maxx = INT_MIN; int in = 0;
+        for(int i=0; i<num.size(); i++){
+            if(i>1 && (num[i]==num[i-1] && num[i]==num[i-2])){
+                if(num[i]-'0' > maxx){
+                    maxx = max(maxx,num[i]-'0');
+                    in = i;
+                }
+            }
         }
-        if(maxx == 0 && flag) return "000";
-        else if(maxx == 0) return "";
-        return to_string(maxx);
+        if(maxx == INT_MIN) return "";
+        return string(3,num[in]);
     }
 };
