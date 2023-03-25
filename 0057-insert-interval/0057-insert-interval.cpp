47ef57d4123a596ebcbd1a40e4e3e986e2facf2d
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-03-26 00:00:41 +0530

    Time: 13 ms (86.20%), Space: 17.2 MB (46.81%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>&no, vector<int>&nI) {
-        vector<vector<int>> ans;
-        no.push_back(nI);
-        sort(no.begin(),no.end());
-        for(int i=0; i<no.size(); i++){
-            int x = no[i][0],y = no[i][1];
-            while(i+1 < no.size() && y >= no[i+1][0]){
-                y = max(y,no[i+1][1]);
+    vector<vector<int>> insert(vector<vector<int>>&v, vector<int>&vv) {
+        vector<vector<int>> ans; int i=0;
+        // if(v.size()==0) return {{vv[0],vv[1]}};
+        // while(i<v.size()){
+            while(i<v.size() && v[i][1] < vv[0]){
+                ans.push_back(v[i]); i++;
+            }
+            while(i<v.size() && vv[1] >= v[i][0]){
+                vv[0] = min(v[i][0],vv[0]);
+                vv[1] = max(v[i][1],vv[1]); 
                 i++;
             }
-            ans.push_back({x,y});
-        }
+            ans.push_back(vv);
+            while(i<v.size()){
+                ans.push_back(v[i]); i++;
+            }
+        // }
         return ans;
     }
 };
