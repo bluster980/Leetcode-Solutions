d30ee39cfb1a69f78c5b7b598be9301a6529b905
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-03-28 23:16:34 +0530

    Time: 288 ms (72.46%), Space: 110.3 MB (79.27%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int distributeCandies(vector<int>& cT) {
-        set<int> mp;
-        for(int i=0; i<cT.size(); i++){
-            mp.insert(cT[i]);
-            // mp[cT[i]]++;
-        }
-        return min(mp.size(),cT.size()/2);
+        return min(unordered_set<int> (begin(cT),end(cT)).size(),cT.size()/2);
+        
     }
 };
