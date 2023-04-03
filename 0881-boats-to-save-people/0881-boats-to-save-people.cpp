6c94f374ac2db85cfdac48fc0dcde2df329fece7
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-04-03 19:46:10 +0530

    Time: 98 ms (18.59%), Space: 41.9 MB (59.84%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int count=0;
+        for(int i=0; i<people.size(); i++)
+            if(i==people.size()) {break;}
+            else if(people[i]+people.back() <= limit && (i!=people.size())){
+                count++;
+                people.pop_back();
+            
+            }
+            else{
+                while(people.size()>i && people[i]+people.back()>limit && (i!=people.size())){
+                    count++;
+                    // i++;
+                    people.pop_back();
+                    
+                }
+                
+                // cout<<people.back()<<" ";
+                
+                if(people.size()>i) {count++; people.pop_back();}
+                // else{
+                    // count++;
+                // }
+            }           
+            return count;   
+        }
+};
