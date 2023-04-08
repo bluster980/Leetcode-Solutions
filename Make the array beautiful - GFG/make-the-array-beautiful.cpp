e7b0b1012791088d3808da92fc53aa7057bf7e2e
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-04-09 00:24:28 +0530

    Added solution - LeetHub

--- Make the array beautiful - GFG/make-the-array-beautiful.cpp
+++ Make the array beautiful - GFG/make-the-array-beautiful.cpp
@@ -10,17 +10,22 @@ using namespace std;
 
 class Solution {
   public:
-    vector<int> makeBeautiful(vector<int> arr) {
-        vector<int> res;
-        for(int i =0;i<arr.size();i++){
-            if(res.size() == 0)res.push_back(arr[i]);
-            else{
-                if((res.back()>0 && arr[i]<0) or (res.back()<0 && arr[i]>0) or 
-                (arr[i] == 0 and res.back()<0) or (res.back() == 0 and arr[i]<0))res.pop_back();
-                else res.push_back(arr[i]);
-            }
+    vector<int> makeBeautiful(vector<int> v) {
+        stack<int> st;
+        for (int i = 0; i < v.size(); i++)
+    	{
+    		while(i<v.size() && !st.empty() && ((st.top()<0 && v[i]>=0) || (st.top()>=0 && v[i]<0))){
+    			i++; st.pop();
+    		}
+    		if(i<v.size()) st.push(v[i]);
+    	}
+        vector<int> ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
         }
-        return res;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
 
