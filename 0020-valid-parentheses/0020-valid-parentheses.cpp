f2ba3233e6e166c5089b0997ed66349a1014fd5e
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-04-10 21:59:05 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (56.25%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-      
         for(auto x:s){
             if(x=='{' || x=='(' || x=='['){
                 st.push(x);
@@ -11,22 +10,10 @@ public:
                 if(st.empty()) {return false;}
                 char temp = st.top();
                 if((x=='}' && temp=='{') || (x==']' && temp=='[') || (x==')' && temp=='(')) st.pop();
-                //     if(st.top()=='{') st.pop();
-                //     else {return false;}
-                // }
-                // else if(x==']'){
-                //     if(st.top()=='[') st.pop();
-                //     else {return false;}
-                // }
-                // else if(x==')'){
-                //     if(st.top()=='(') st.pop();
-                //     else {return false;}
-                // }
                 else return false;
                 
             }
         }
-        if(st.empty()) {return true;}
-        return false;
+        return st.empty();
     }
 };
