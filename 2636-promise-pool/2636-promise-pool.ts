9f747c3149deb266ec770fa1c7367b2b6d1dcd38
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-05-17 23:27:52 +0530

    Time: 85 ms (7.04%), Space: 43.4 MB (35.21%) - LeetHub

--- 2636-promise-pool/2636-promise-pool.ts
+++ 2636-promise-pool/2636-promise-pool.ts
@@ -1,16 +1,6 @@
-/**
- * @param {Function[]} functions
- * @param {number} n
- * @return {Function}
- */
-var promisePool = async function(functions, n) {
-    let next = () => functions[n++]?.().then(next);
-    return Promise.all(functions.slice(0, n).map(f => f().then(next)));
+type F = () => Promise<any>
 
-};
-
-/**
- * const sleep = (t) => new Promise(res => setTimeout(res, t));
- * promisePool([() => sleep(500), () => sleep(400)], 1)
- *   .then(console.log) // After 900ms
- */
+const promisePool = (fn: F[], n: number): Promise<any> => {
+    let next = () => fn[n++]?.().then(next)
+    return Promise.all(fn.slice(0, n).map(f => f().then(next)))
+}
