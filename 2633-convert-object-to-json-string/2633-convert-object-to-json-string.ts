b63a57d7fbbca897bca6da1eddd57083e7476444
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-05-22 23:16:22 +0530

    Time: 79 ms (79.94%), Space: 48.7 MB (83.05%) - LeetHub

--- 2633-convert-object-to-json-string/2633-convert-object-to-json-string.ts
+++ 2633-convert-object-to-json-string/2633-convert-object-to-json-string.ts
@@ -1,20 +1,34 @@
-/**
- * @param {any} object
- * @return {string}
- */
-var jsonStringify = function(object) {
-  if (object === null) return 'null';
-  if (typeof object === 'boolean' || typeof object === 'number') return object.toString();
-  if (typeof object === 'string') return '"' + object.replace(/[\\"\n\r\t]/g, char => escapeMap[char]) + '"';
-  if (Array.isArray(object)) return '[' + object.map(jsonStringify).join(',') + ']';
-  if (typeof object === 'object') return '{' + Object.keys(object).map(key => jsonStringify(key) + ':' + jsonStringify(object[key])).join(',') + '}';
-  return '';
-};
+function jsonStringify(object: any): string {
+    if (typeof object === 'string') return `"${object}"`;
+    if (typeof object === 'boolean') return object ? "true" : "false";
+    if (typeof object === 'number') return String(object);
+    if (object === null) return "null";
+    
+    if (Array.isArray(object)) {
+        let string = "[";
+        for (let i = 0; i < object.length; i++) {
+            if (i === object.length - 1) {
+                string += jsonStringify(object[i]);
+                break;
+            }
+            string += `${jsonStringify(object[i])},`;
+        }
+        string += "]";
+        return string;
+    }
 
-const escapeMap = {
-  '"': '\\"',
-  '\\': '\\\\',
-  '\n': '\\n',
-  '\r': '\\r',
-  '\t': '\\t'
+    if (typeof object === "object") {
+        let string = "{"
+        const filteredEntries = Object.entries(object).filter(v => v[1] !== undefined)
+        for (let i = 0; i < filteredEntries.length; i++) {
+            if (i === filteredEntries.length - 1) {
+                string += `"${filteredEntries[i][0]}":${jsonStringify(filteredEntries[i][1])}`
+                break;
+            }
+            string += `"${filteredEntries[i][0]}":${jsonStringify(filteredEntries[i][1])},`
+        }
+        string += "}";
+        return string;
+    }
+
 };
