3d1574c3734c41e3ab1a70e510dae386d7d38519
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-05-25 21:50:42 +0530

    Time: 81 ms (10.79%), Space: 46.2 MB (30.58%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.ts
@@ -0,0 +1,11 @@
+const chunk = (arr, size) => {
+  return arr.reduce((chunkedArray, element) => {
+    const lastChunk = chunkedArray[chunkedArray.length - 1];
+    if (!lastChunk || lastChunk.length === size) {
+      chunkedArray.push([element]);
+    } else {
+      lastChunk.push(element);
+    }
+    return chunkedArray;
+  }, []);
+};
