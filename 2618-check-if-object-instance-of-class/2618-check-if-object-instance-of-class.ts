c1be1c8c4cc9c76362bf96bd756fe059c3d30b14
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-05-29 21:29:14 +0530

    Time: 111 ms (60.26%), Space: 52.6 MB (53.99%) - LeetHub

---
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.ts
@@ -0,0 +1,4 @@
+function checkIfInstanceOf(obj: any, classFunction: any): boolean {
+ if (obj == null || typeof classFunction !== 'function' ) return false;
+    return Object(obj) instanceof classFunction;
+};
