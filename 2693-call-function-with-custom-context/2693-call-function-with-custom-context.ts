e570474481ee50648e7158a51d803790e72f6484
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-05-30 22:23:08 +0530

    Time: 66 ms (48.94%), Space: 44.7 MB (43.62%) - LeetHub

--- 2693-call-function-with-custom-context/2693-call-function-with-custom-context.ts
+++ 2693-call-function-with-custom-context/2693-call-function-with-custom-context.ts
@@ -1,3 +1,9 @@
+declare global { 
+    interface Function {
+      callPolyfill(context: Record<any, any>, ...args: any[]): any;
+	}
+}
+
 Object.defineProperty(Function.prototype, 'callPolyfill', {
   value: function(obj, ...args) {
     Object.defineProperty(obj, '__temp__', {
@@ -13,3 +19,7 @@ Object.defineProperty(Function.prototype, 'callPolyfill', {
   },
   enumerable: false
 });
+/**
+ * function increment() { this.count++; return this.count; }
+ * increment.callPolyfill({count: 1}); // 2
+ */
