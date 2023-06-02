becadad752b0f62d3dc0cd7606d7ac18479289ea
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-06-02 22:42:19 +0530

    Time: 55 ms (86.29%), Space: 42.8 MB (73.81%) - LeetHub

---
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.ts
@@ -0,0 +1,16 @@
+function* fibGenerator(): Generator<number, any, number> {
+    // fib(0) === 0
+    yield 0;
+    // fib(1) === 1
+    yield 1;
+    
+    let a: number = 0;
+    let b: number = 1;
+    
+    while (true) {
+        const c: number = a + b;
+        a = b;
+        b = c;
+        yield c;
+    }
+};
