3e94e51dcddd6dd7b9182857d857762dc3e2e8b8
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-06-11 23:52:07 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (15.19%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     string mergeAlternately(string w1, string w2) {
-        string res;
-        for (auto i = 0; i < max(w1.size(), w2.size()); ++i) {
-            if (i < w1.size())
-                res += w1[i];
-            if (i < w2.size())
-                res += w2[i];
+        string ans; int maxx = max(w1.size(),w2.size());
+        for(int i=0; i<maxx; i++){
+            if(i<w1.size()){
+                ans.push_back(w1[i]);
+            }
+            if(i<w2.size()){
+                ans.push_back(w2[i]);
+            }
         }
-        return res;
+        return ans;
     }
 };
