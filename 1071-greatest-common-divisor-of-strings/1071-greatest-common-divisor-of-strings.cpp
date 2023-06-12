b2b3fd1d1a8ee7ecf2483a61b2f9a0d68015ea10
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-06-12 11:41:46 +0530

    Time: 3 ms (64.02%), Space: 6.9 MB (90.01%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        int x = __gcd(str1.size(),str2.size());
-        string temp = str2;
-        str2+=str1; str1+=temp;
-        string ans;
-        for(int i=0; i<x; i++){
-            ans.push_back(str1[i]);
-        }
-        if(str1==str2) return ans;
-        return "";
+        return (str1+str2==str2+str1)?str1.substr(0,__gcd(str1.size(),str2.size())):"";
     }
 };
