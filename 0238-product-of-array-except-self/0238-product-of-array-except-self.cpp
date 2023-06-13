01d0b6a51d882055eb073a0e5fd02aaa6765efc4
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-06-13 20:37:04 +0530

    Time: 27 ms (69.15%), Space: 24 MB (62.45%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> ans(nums.size(),1); 
-        vector<int> first(nums.size(),1),second(nums.size(),1);
         for(int i=1; i<nums.size(); i++){
-            first[i] = first[i-1] * nums[i-1];
+            ans[i] = ans[i-1] * nums[i-1];
         }
-        for(int i=nums.size()-2; i>=0; i--){
-            second[i] = second[i+1] * nums[i+1];
+        int curr = nums[ans.size()-1];
+        for(int i=ans.size()-2; i>=0; i--){
+            ans[i] = curr*ans[i];
+            curr *= nums[i];
+            
         }
-        for(int i=0; i<nums.size(); i++){
-            ans[i] = second[i]*first[i];
-        }
+        //you can run two for loop and store two dif array then multiple and make ans array it is also right but O(n) space complex...
         return ans;
         
     }
