160352a0ebb435c23826a0e50f4f34081530ee5b
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-06-20 13:39:29 +0530

    Time: 267 ms (38.83%), Space: 139 MB (28.37%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-        if(nums.size() < k*2 + 1) return vector<int> (nums.size(),-1);
-        vector<int> ans(nums.size());
-        int i=0;
-        for(i=0; i<k; i++){
-            ans[i] = -1;
-            ans[nums.size()-1 - i] = -1;
-        }
-        long long temp = 0;
-        for(int j=0; j<=i+k; j++){
-            temp += nums[j];
-        }
-        ans[i] = temp/(2*k+1); i++;
-        while(i < nums.size()-k){
-            temp = (temp-nums[i-k-1]+nums[i+k]);
-            ans[i] = temp/(2*k+1);
-            i++;
+        vector<int> ans(nums.size(),-1); int x = 0; vector<long long> dumy(nums.size(),-1);
+        if(nums.size() < (k*2 + 1)) return ans; dumy[0]=nums[0];
+        for(int i=1; i<nums.size(); i++) {dumy[i] = nums[i] + dumy[i-1];}
+        ans[k] = dumy[2*k]/(2*k+1);
+        for(int j=2*k+1; j<nums.size(); j++){
+            ans[k+x+1] = (dumy[j]-dumy[x])/(2*k+1); x++;
         }
         return ans;
     }
