0940df843f6c22dd8996beb8e6b60298dd5138be
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-06-21 21:41:31 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (7.01%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -3,7 +3,6 @@ public:
     int strStr(string h, string n) {
         int i=0,j=n.size()-1;
         while(i<h.size()){
-        	// cout<<h.substr(i,j+1)<<endl;
             if(h.substr(i,j+1) == n){
                 return i;
             }
