0bc454141b75c78e6a06bc3ef62d73ac078f2a7b
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-07-03 16:39:38 +0530

    Time: 4 ms (54.81%), Space: 6.9 MB (52.51%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string g) {
+        if(s.size() == 1) return false;
+        map<char,int> mp;
+        for(int i=0; i<s.size(); i++){
+            mp[s[i]]++;
+        }
+        int first = INT_MIN,second = INT_MIN;
+        for(int i=0; i<s.size(); i++){
+            if(s[i] != g[i]){
+                if(first == INT_MIN) {first = i;}
+                else {second = i; break;}
+            }
+        }
+        // cout<<first<<" "<<second<<endl;
+        if(first!=INT_MIN && second!=INT_MIN){
+            swap(s[first],s[second]);
+            return s==g;
+        }
+        else if(s==g){
+            return (mp.size()<s.size());
+        }
+        return false;
+    }
+};
