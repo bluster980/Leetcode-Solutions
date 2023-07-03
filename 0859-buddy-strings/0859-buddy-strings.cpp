9554d4b6244517f84e06b9afc689ea32ba29777d
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-07-03 16:45:51 +0530

    Time: 4 ms (54.81%), Space: 7.1 MB (19.51%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.cpp
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -3,16 +3,16 @@ public:
     bool buddyStrings(string s, string g) {
         if(s.size() == 1) return false;
         unordered_set<char> st;
-        for(int i=0; i<s.size(); i++){
-            st.insert(s[i]);
-        }
         int first = INT_MIN,second = INT_MIN;
         for(int i=0; i<s.size(); i++){
+            st.insert(s[i]);
             if(s[i] != g[i]){
                 if(first == INT_MIN) {first = i;}
                 else {second = i; break;}
             }
         }
+        // for(int i=0; i<s.size(); i++){
+        // }
         if(first!=INT_MIN && second!=INT_MIN){
             swap(s[first],s[second]);
             return s==g;
