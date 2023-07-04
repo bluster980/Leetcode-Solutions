a3bd27e92fd7650880a34507b033ff8e52485236
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-07-04 22:16:14 +0530

    Time: 20 ms (75.31%), Space: 17 MB (29.87%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         for(int i=1; i<nums.size(); i++) nums[i]^=nums[i-1];
         return nums.back();
     }
