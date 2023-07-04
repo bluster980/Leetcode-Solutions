2651a59c383e6856d5749a7912ea1fe5a33da633
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-07-04 20:12:50 +0530

    Time: 14 ms (5.91%), Space: 13.1 MB (73.06%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void tra(TreeNode* root, multimap<map<int,int>,int> &mp){
+        queue<pair<int,TreeNode*>> q; int level = 0, x = 0;
+        q.push({0,root}); q.push({0,NULL});
+        while(!q.empty()){
+            int x = q.front().first;
+            TreeNode* temp = q.front().second; q.pop();
+            if(temp == NULL){
+                if(q.empty()) break;
+                q.push({0,NULL}); level++;
+            }
+            else{
+                map<int,int> go; go.clear(); go.insert({x,level});
+                mp.insert({go,temp->val});
+                // mp[{x,level}] = temp->val;
+                // cout<<x<<" "<<level<<" "<<temp->val<<endl;
+                if(temp->left) q.push({x-1,temp->left});
+                if(temp->right) q.push({x+1,temp->right});
+            }
+            
+        }
+        
+    }
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        if(root == NULL) return vector<vector<int>> {};
+        multimap<map<int,int>,int> mp; 
+        vector<vector<int>> ans;
+        tra(root,mp); 
+        // multimap<int,int> dumy;
+        // for(auto xx:mp){
+        //     dumy.insert({xx.first.first,xx.second});
+        // }
+        // map<int,int> mpppp; mpppp.insert({-2,2}); mp.insert({mpppp,3});
+        vector<int> yui; int prev;
+        vector<pair<map<int, int>, int>> vec(mp.begin(), mp.end());
+        sort(vec.begin(),vec.end()); int tt = 0,temm=1; 
+        for (auto pair : vec) {
+            for (auto element : pair.first) {
+                if(tt && prev != element.first){
+                    ans.push_back(yui); yui.clear(); prev = element.first;
+                }
+                yui.push_back(pair.second);
+                if(temm--) {prev = element.first; tt++;}
+                // cout << element.first <<" "<< element.second<<" ";
+            }
+            // cout << pair.second << std::endl;
+        }
+        ans.push_back(yui);
+        return ans;
+    }
+};
