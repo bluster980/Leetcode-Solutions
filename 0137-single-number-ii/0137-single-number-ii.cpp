b5cae61c68fd1aa3dff667a431cae239a3371777
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-07-05 00:22:28 +0530

    Time: 14 ms (20.77%), Space: 9.4 MB (98.52%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        vector<int> bitset(32,0);
-        for(int &n:nums) {
-            int mask = 0;
-            while(mask<32) {
-                if((n&(1<<mask))) {
-                    bitset[mask]++;
-                }
-                mask++;
+        vector<int> v(32,0);
+        for(int i=0; i<nums.size(); i++){
+            for(int j=0; j<32; j++){
+                if((1<<j)&nums[i]) v[j]++;
             }
         }
-        int nm  = 0;
-        for(int i = 0; i < 32; i++) {
-            if(bitset[i]%3) {
-                nm |= (1 <<i);
-            }   
+        int temp = 0;
+        for(int i=0; i<v.size(); i++){
+            if(v[i]%3 != 0) temp |= (1<<i);
         }
-        return nm;
+        return temp;
     }
 };
+
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         vector<int> bitset(32,0);
+//         for(int &n:nums) {
+//             int mask = 0;
+//             while(mask<32) {
+//                 if((n&(1<<mask))) {
+//                     bitset[mask]++;
+//                 }
+//                 mask++;
+//             }
+//         }
+//         int nm  = 0;
+//         for(int i = 0; i < 32; i++) {
+//             if(bitset[i]%3) {
+//                 nm |= (1 <<i);
+//             }   
+//         }
+//         return nm;
+//     }
+// };
