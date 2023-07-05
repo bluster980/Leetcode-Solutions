b368616954859705215dd6b33387c94760eee80e
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-07-06 01:14:29 +0530

    Time: 9 ms (23.89%), Space: 8.8 MB (53.56%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int maxx = nums[0]; int in = 0;
-        for(int i=1; i<nums.size()-1; i++){
-            if(nums[i] > nums[i-1] && nums[i]>nums[i+1]){
-                if(nums[i] > maxx){
-                    in = i;
-                    maxx = nums[i];
-                }
+        if(nums.size()==1 || (nums[0]>nums[1])) return 0;
+        if(nums.size()==2) return (nums[0]>nums[1])?0:1;
+        if(nums[nums.size()-1] > nums[nums.size()-2]) return nums.size()-1;
+        int l=1,r=nums.size()-2;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(mid>0 && mid<nums.size()-1 && nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                return mid;
             }
+            else if(nums[mid] < nums[mid-1]) r=mid-1;
+            else if(nums[mid] < nums[mid+1]) l=mid+1;
         }
-        if(nums[nums.size()-1] > maxx){
-            in = nums.size()-1;
-            maxx = nums[in];
-        }
-        return in;
+        return -1;
     }
 };
