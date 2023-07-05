18d21c3532a1a0fd568dac781137c778979894f5
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-07-05 17:40:09 +0530

    Time: 239 ms (37.17%), Space: 102.9 MB (64.58%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -4,11 +4,7 @@ public:
         vector<int> v(t.size(),0); stack<int> st; st.push(0);
         for(int i=1; i<t.size(); i++){
             while(!st.empty() && t[st.top()] < t[i]){
-                int temp = st.top();
-                if(t[temp] < t[i]){
-                    v[temp] = i-temp;
-                }
-                st.pop();
+                v[st.top()] = i-st.top(); st.pop();
             }
             st.push(i);
         }
