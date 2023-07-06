8b92071324465a1e28f04adee906ea4405190e6d
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-07-06 12:04:38 +0530

    Time: 5 ms (42.67%), Space: 6.5 MB (64.58%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
+    bool check(string s){
+        int n=s.size();
+        if(n==1){
+            return true;
+        }
+        if(n>3||s[0]=='0'){
+            return false;
+        }
+        int val=stoi(s);
+        if(val>255){
+            return false;
+        }
+        return true;
+    }
     vector<string> restoreIpAddresses(string s) {
-        vector<string> ret;
-        string ans;
-        
-        for (int a=1; a<=3; a++)
-        for (int b=1; b<=3; b++)
-        for (int c=1; c<=3; c++)
-        for (int d=1; d<=3; d++)
-            if (a+b+c+d == s.length()) {
-                int A = stoi(s.substr(0, a));
-                int B = stoi(s.substr(a, b));
-                int C = stoi(s.substr(a+b, c));
-                int D = stoi(s.substr(a+b+c, d));
-                if (A<=255 && B<=255 && C<=255 && D<=255)
-                    if ( (ans=to_string(A)+"."+to_string(B)+"."+to_string(C)+"."+to_string(D)).length() == s.length()+3)
-                        ret.push_back(ans);
-            }    
-        
-        return ret;
+        int n=s.size();
+        vector<string>ans;
+        if(n>12){
+            return ans;
+        }
+        for(int i=1;i<=3;i++){
+            for(int j=1;j<=3;j++){
+                for(int k=1;k<=3;k++){
+                    if(i+j+k<n&&i+j+k+3>=n){
+                        string a=s.substr(0,i);
+                        string b=s.substr(i,j);
+                        string c=s.substr(j+i,k);
+                        string d=s.substr(i+j+k);
+                        if(check(a)&&check(b)&&check(c)&&check(d)){
+                            ans.push_back(a+"."+b+"."+c+"."+d);
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
     }
 };
