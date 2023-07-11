e2e6dd261f4d46180190034fd0a7252bf7b2883c
Jatin-parmar <79743138+Jatin-parmar@users.noreply.github.com>
2023-07-11 15:54:42 +0530

    Time: 44 ms (6.34%), Space: 18 MB (5.01%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    vector<int>vis,col;
-    bool dfs(int v, int c, vector<vector<int>>& graph){
-        vis[v]=1;
-        col[v]=c;
-        for(int child:graph[v]){
-            if(vis[child]==0){
-                if(dfs(child,c^1,graph)==false) 
-                    return false;
-            }
-            else{
-                if(col[v]==col[child])
-                    return false;
-            }
+    bool dfs(int vertex,int col,vector<int> g[],vector<int> &color){
+        color[vertex] = col;
+        bool isCol = true;
+        for(auto child:g[vertex]){
+            if(color[child] == col) return false;
+            else if(color[child] == -1) isCol &= dfs(child,!col,g,color);
         }
-        return true;
+        return isCol;
     }
-    
     bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vis.resize(n);
-        col.resize(n);
-
-        for(int i=0;i<n;++i){
-            if(vis[i]==0 && dfs(i,0,graph)==false){ 
-                return false;
+        vector<int> color(graph.size(),-1);
+        vector<int> g[graph.size()];
+        for(int i=0; i<graph.size(); i++){
+            for(int j=0; j<graph[i].size(); j++){
+                g[i].push_back(graph[i][j]);
+                g[graph[i][j]].push_back(i);
             }
         }
-        
-        return true;
+        bool isCol = true; int col = 1;
+        for(int i=0; i<graph.size(); i++){
+            if(color[i] == -1){
+                isCol &= dfs(i,col,g,color);
+            }
+        }
+        return isCol;
     }
 };
