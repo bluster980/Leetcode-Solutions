8fddb6c86d400d2598217c0328d98c204a53b9cd
Jatin Parmar <79743138+bluster980@users.noreply.github.com>
2023-07-19 11:07:08 +0530

    Time: 49 ms (6.42%), Space: 6.1 MB (24.66%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
-    int fib(int n) {
+    int fibon(int n,vector<int> &dp){
         if(n==0 || n==1) return n;
-        return fib(n-1)+fib(n-2);
+        dp[n] = fibon(n-1,dp)+fibon(n-2,dp);
+        return dp[n];
+    }
+    int fib(int n) {
+        vector<int> dp(n+1);
+        return fibon(n,dp);
     }
 };
