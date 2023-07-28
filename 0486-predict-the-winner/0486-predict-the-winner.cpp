528c58fe96a0b2e3694638bc3cfe4245ee1b9d91
Jatin Parmar <79743138+bluster980@users.noreply.github.com>
2023-07-28 11:02:45 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (75.78%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool PredictTheWinner(vector<int>& nums) {
+        vector<int> dp(nums);
+        for(int len = 2;len<=nums.size();len++){
+            for(int i=0;i+len-1<nums.size();i++){
+                dp[i] =  max(nums[i]-dp[i+1], nums[i+len-1]-dp[i]);
+            }
+        }
+        return dp[0] >= 0;
+    }
+};
