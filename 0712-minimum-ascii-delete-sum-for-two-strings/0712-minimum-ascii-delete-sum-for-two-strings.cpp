3b84d4f99c3027148a720ccc8acae0e14cf67f1f
Jatin Parmar <79743138+bluster980@users.noreply.github.com>
2023-07-31 22:08:24 +0530

    Time: 35 ms (82.66%), Space: 15.4 MB (14.89%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -18,8 +18,7 @@ public:
                 if(a[i-1] == b[j-1])
                     dp[i][j] = dp[i-1][j-1];
                 else
-                    dp[i][j] = min( dp[i-1][j] + a[i-1] ,
-                                    dp[i][j-1] + b[j-1] );
+                    dp[i][j] = min( dp[i-1][j] + a[i-1],dp[i][j-1] + b[j-1] );
             }
         }
         return dp[n][m];
