35741a8c80e42ceed4cbb557ec1212d1caae1584
Jatin Parmar <79743138+bluster980@users.noreply.github.com>
2023-08-01 14:58:16 +0530

    Time: 0 ms, Memory: 9.6 MB - LeetHub

--- duplicate-zeros/duplicate-zeros.cpp
+++ duplicate-zeros/duplicate-zeros.cpp
@@ -1,15 +1,30 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-        vector<int> ans;
-        for(int i=0; i<arr.size(); i++){
+        int cnt = 0; bool flag = false;
+        for(int i=0; cnt+i<arr.size(); i++){
             if(arr[i]==0){
-                ans.push_back(0);
-                if(arr.size() == ans.size()) break;
+                cnt++;
             }
-            ans.push_back(arr[i]);
-            if(arr.size() == ans.size()) break;
+            if(cnt+1+i > arr.size()) {
+                cnt--; flag = true;
+            }
         }
-        arr = ans;
+        cout<<cnt<<endl;
+        cout<<flag<<endl;
+        
+        if(cnt != arr.size() && cnt!=0){
+            int j = arr.size()-cnt-1; int x = arr.size()-1;
+            if(flag){
+                arr[x] = 0;
+                x--; j--;
+            }
+            for(int i=j; i>=0; i--){
+                if(i<arr.size() && arr[i] == 0){
+                    arr[x] = 0; x--;
+                }
+                arr[x] = arr[i]; x--;
+            }
+        }
     }
 };
