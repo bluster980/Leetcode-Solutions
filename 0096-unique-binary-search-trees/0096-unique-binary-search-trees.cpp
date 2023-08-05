5a4ed08310cc82db0ed3652f0a0b47259b278e81
Jatin Parmar <79743138+bluster980@users.noreply.github.com>
2023-08-05 21:32:13 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (10.18%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
+    unordered_map<int,int> dp;
     int numTrees(int n) {
         if(n==0 || n==1) return 1;
+        if(dp[n]!=0) return dp[n];
         int result = 0;
         for(int i=1; i<=n; i++){
             int l = numTrees(i-1);
             int r = numTrees(n-i);
             result += l*r;
         }
-        return result;
+        return dp[n] = result;
     }
 };
