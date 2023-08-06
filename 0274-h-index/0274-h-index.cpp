f1a77ab8e45d0a2e23872a4d5603e78f8d59a57a
Jatin Parmar <79743138+bluster980@users.noreply.github.com>
2023-08-06 22:45:26 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (11.34%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int hIndex(vector<int>& c) {
-        vector<int> v(c.size()+1,0);
+        vector<int> v(c.size()+1,0); int count = 0;
         for(int i=0; i<c.size(); i++){
             if(c[i] < c.size()) v[c[i]]++;
             else v[c.size()]++;
         }
-        int count = 0;
         for(int i=c.size(); i>=0; i--){
             count += v[i];
             if(count >= i){
