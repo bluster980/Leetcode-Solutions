1f12f6d957ee3978489256e34ca7812e9e6772ac
Jatin Parmar <79743138+bluster980@users.noreply.github.com>
2023-08-08 14:56:48 +0530

    Time: 15 ms (99.36%), Space: 24.9 MB (62.70%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if(k > nums.size()) k%=nums.size();
+        reverse(nums.begin(),nums.end()-k);
+        reverse(nums.end()-k,nums.end());
+        reverse(nums.begin(),nums.end());
+    }
+};
