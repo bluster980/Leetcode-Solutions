29006a1bb7261cf7645a2a8cd6cf8ac235674d40
Jatin Parmar <79743138+bluster980@users.noreply.github.com>
2023-09-05 17:18:05 +0530

    Time: 458 ms (70.57%), Space: 164.6 MB (92.20%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,67 +1,61 @@
-class Node{
-    public:
-    int val;
-    Node *nxt,*prv;
-    Node():val(-1),prv(nullptr),nxt(nullptr){}
-};
-
+#define ff first
+#define ss second
 class LRUCache {
 public:
-    LRUCache(int capacity):capacity(capacity) {
-        head->nxt = tail;
-        tail->prv = head;
+    map<int,int>kp,m; int count = 0;
+    //key, counter in queue
+    queue<pair<int,int>> q;
+    int capacity;
+    LRUCache(int capacity) {
+        this->capacity = capacity;
     }
     
     int get(int key) {
-        if(Arr[key].val == -1) return -1;
-        else{
-            to_tail(&Arr[key]);
-            return Arr[key].val;
-        }
+        
+        if(m[key]==0)
+            return -1;
+        
+        q.push({key,++m[key]});
+        return kp[key];
+    
     }
     
     void put(int key, int value) {
-        if(Arr[key].val != -1){
-            to_tail(&Arr[key]);
-            Arr[key].val = value;
-            return;
+        kp[key] = value;
+        
+        if(m[key]>0)
+        {
+            q.push({key,++m[key]});
+            return ;
         }
-        else{
-            if(len+1<=capacity){   
-                insert(&Arr[key]);
-               len++;
-                Arr[key].val = value;
-            }
-            else{
+        
+        if(count<capacity)
+        {
+            if(m[key]==0 || m.count(key)==0)
+                count++;
+            q.push({key,++m[key]});
+            return ;
+        }
             
-                head->nxt->val = -1;
-                remove(head->nxt);
-
-                Arr[key].val = value;
-                insert(&Arr[key]);
+        while(1)
+        {
+            pair<int,int> tem = q.front();
+            q.pop();
+            if(m[tem.ff]==tem.ss)
+            {
+                m[tem.ff] = 0;
+                q.push({key,++m[key]});
+                return ;
             }
         }
+        
+        
     }
-
-    void to_tail(Node* cur){
-        remove(cur);
-        insert(cur);
-    }
-
-    void remove(Node* cur){
-        cur->nxt->prv = cur->prv;
-        cur->prv->nxt = cur->nxt;
-    }
-
-    void insert(Node* cur){
-        tail->prv->nxt = cur;
-        cur->prv = tail->prv;
-
-        cur->nxt = tail;
-        tail->prv = cur;
-    }
-
-    array<Node,10005> Arr;
-    int len = 0,capacity;
-    Node* head = new Node,*tail = new Node;
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
