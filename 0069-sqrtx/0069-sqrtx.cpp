142149bec427bf146b0629c7595871369af668b6
Jatin Parmar <79743138+bluster980@users.noreply.github.com>
2023-09-13 23:00:37 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (13.12%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x>0 && x < 4) return 1;
+        if(x > 0 && x < 4) return 1;
         int l = 1, r = x/2;
         while(l <= r){
             int mid = l+(r-l)/2;
-            // cout<<mid<<endl;
             if(1ll*mid*mid == x){
                 return mid;
             }
