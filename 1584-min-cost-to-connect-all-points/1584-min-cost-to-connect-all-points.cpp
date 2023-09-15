63db3879369e1dadd0bf1c7b5b8b3afba4b31e1d
Jatin Parmar <79743138+bluster980@users.noreply.github.com>
2023-09-15 21:13:15 +0530

    Time: 383 ms (64.68%), Space: 58.6 MB (54.90%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,20 +1,58 @@
+class DisjointSetUnion {
+    vector<int> parent, size;
+    public:
+    DisjointSetUnion(int n) {
+        size.resize(n+1, 1);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findParent(int node) {
+        if(node == parent[node]) {
+            return node;
+        }
+        return parent[node] = findParent(parent[node]);
+    }
+    
+    void unionBySize(int u, int v) {
+        int pu = findParent(u);
+        int pv = findParent(v);
+        if(pu == pv) return;
+
+        if(size[pu] < size[pv]) {
+            size[pv] += size[pu];
+            parent[pu] = pv;
+        }
+        else {
+            size[pu] += size[pv];
+            parent[pv] = pu;
+        }
+    }
+
+};
+
+
 class Solution {
 public:
-    int minCostConnectPoints(vector<vector<int>>& ps) {
-    int n = ps.size(), res = 0, i = 0, connected = 0;
-    vector<bool> visited(n);
-    priority_queue<pair<int, int>> pq;
-    while (++connected < n) {
-        visited[i] = true;
-        for (int j = 0; j < n; ++j)
-            if (!visited[j])
-                pq.push({-(abs(ps[i][0] - ps[j][0]) + abs(ps[i][1] - ps[j][1])), j});
-        while (visited[pq.top().second])
-            pq.pop();
-        res -= pq.top().first;
-        i = pq.top().second;
-        pq.pop();
-    }
-    return res;
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        vector<pair<int, pair<int, int>>> dist;
+        for(int i=0;i<points.size();i++) {
+            for(int j= i+1;j<points.size();j++) {
+                int manhattan_distance = abs(points[j][0]-points[i][0]) + abs(points[j][1] - points[i][1]);
+                dist.push_back({manhattan_distance, {i, j}});
+            }
+        }
+        sort(dist.begin(), dist.end());
+        int cost = 0;
+        DisjointSetUnion dsu(points.size() + 1); 
+        for(int i=0;i<dist.size();i++) {
+            if(dsu.findParent(dist[i].second.first) != dsu.findParent(dist[i].second.second)) {
+                cost += dist[i].first;
+                dsu.unionBySize(dist[i].second.first, dist[i].second.second);
+            }
+        }
+        return cost;
     }
 };
