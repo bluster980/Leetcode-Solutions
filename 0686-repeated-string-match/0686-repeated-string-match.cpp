11ee6df01364c0b3912128979ba7745167c7196a
Jatin Parmar <79743138+bluster980@users.noreply.github.com>
2023-09-28 19:10:31 +0530

    Time: 103 ms (21.13%), Space: 516.7 MB (5.01%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
+    bool isSubString(string x, string y){
+        if(x.find(y) != string::npos){
+            return 1;
+        }
+        return 0;
+    }
     int repeatedStringMatch(string a, string b) {
-        
-        string s = "";
-        int count = 0;
-        while(s.size() < b.size()){
-            s = s + a;
-            count++;
+        string temp = a; int cnt = 1;
+        while(temp.size() < b.size()){
+            temp = temp + a; cnt++;
+            if(isSubString(temp,b)) return cnt;
         }
-        if(s.find(b) != string:: npos){
-            return count;
-        }
-        s = s + a;
-        count++;
-        if(s.find(b) != string:: npos){
-            return count;
-        }
-            return -1;
+        if(isSubString(temp,b)) return cnt;
+        temp = temp+a;
+        if(isSubString(temp,b)) return cnt+1;
+        return -1;
     }
 };
