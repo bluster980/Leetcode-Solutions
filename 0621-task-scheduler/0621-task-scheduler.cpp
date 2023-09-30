5d8bb0357e3ad6d430848b6d2065915bf2389eaa
Jatin Parmar <79743138+bluster980@users.noreply.github.com>
2023-09-30 22:20:07 +0530

    Time: 46 ms (89.00%), Space: 34.7 MB (58.88%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        if(n==0) return tasks.size();
-        vector<int> frequency(26);
-        int maxx = 0;
+        vector<int> v(26); int maxx = 0;
         for(int i=0; i<tasks.size(); i++){
-            frequency[tasks[i]-'A']++;
-            maxx = max(maxx,frequency[tasks[i]-'A']);
+            v[tasks[i]-'A']++;
+            maxx = max(maxx,v[tasks[i]-'A']);
         }
         int ans = (maxx-1) * (n+1);
         for(int i=0; i<26; i++){
-            if(frequency[i] == maxx){
-                ans++;
-            }
+            if(v[i] == maxx) ans++;
         }
-        return max(ans,(int)tasks.size());
+        return max(ans, (int)tasks.size());
     }
 };
