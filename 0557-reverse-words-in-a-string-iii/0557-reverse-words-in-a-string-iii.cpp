43ee941c45963bfd0e05f71f6ee24e5e03531921
Jatin Parmar <79743138+bluster980@users.noreply.github.com>
2023-10-01 11:41:55 +0530

    Time: 12 ms (83.34%), Space: 11.9 MB (34.40%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     string reverseWords(string s) {
         s.push_back(' '); int i=0;
-        int l,r; string ans;
+        int l; string ans;
         while(i<s.size()){
             l=i;
             while(s[i]!=' '){
                 i++;
             }
-            r=i;
-            for(int j=r; j>=l; j--){
+            for(int j=i; j>=l; j--){
                 ans.push_back(s[j]);
-            }
-            // ans.push_back(' ');
-            i++;
+            } i++;
         }
         ans.erase(ans.begin());
         return ans;
