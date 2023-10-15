250706afeda1175a942e5d3e65833fc753e98c03
Jatin Parmar <jatinparmar980@gmail.com>
2023-10-15 11:09:55 +0530

    Time: 31 ms (64.62%), Space: 32.2 MB (37.10%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& nums, int k, int x) {
-        if(nums.size() == 1) return {nums[0]};
-        map<int,int> mp;
-        for(int i=0; i<nums.size(); i++){
-            mp[nums[i]] = abs(nums[i]-x);
-        }
+        // if(nums.size() == 1) return {nums[0]};
+        // map<int,int> mp;
+        // for(int i=0; i<nums.size(); i++){
+        //     mp[nums[i]] = abs(nums[i]-x);
+        // }
         int l=0,r=nums.size()-1;
         while((r-l) >= k){
-            if(mp[nums[l]] <= mp[nums[r]]){
+            if(abs(nums[l]-x) <= abs(nums[r]-x)){
                 r--;
             }
             else{
