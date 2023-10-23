74a3c71267bb780ac9011d59fffce00da2dfa778
Jatin Parmar <jatinparmar980@gmail.com>
2023-10-23 10:44:02 +0530

    Time: 5 ms (10.09%), Space: 6.2 MB (47.70%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -5,11 +5,16 @@ public:
         if(n==INT_MIN || n<4 && n!=1 ) return false;
         bool ans = false;
         for(int i=0; i<32; i++){
-            if(n & (1<<i))
+            if(n & (1<<i)){
+                if(!(i%2)){
+                    ans = true;
+                }
                 count++;
-            if(n & (1<<i) && i%2==0){
-                ans = true;
             }
+            //     count++;
+            // if(n & (1<<i) && i%2==0){
+            //     ans = true;
+            // }
         }
         return (count==1)?ans:false;
     }
