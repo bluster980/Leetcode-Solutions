28a7445a258a3e248944c063a2ad83308d64458b
Jatin Parmar <jatinparmar980@gmail.com>
2023-10-23 10:37:20 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (47.70%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        int count = 0;
+        if(n==INT_MIN || n<4 && n!=1 ) return false;
+        bool ans = false;
+        for(int i=0; i<32; i++){
+            if(n & (1<<i))
+                count++;
+            if(n & (1<<i) && i%2==0){
+                ans = true;
+            }
+        }
+        return (count==1)?ans:false;
+    }
+};
