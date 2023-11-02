71c384128b2df01b495979c1952b1c9cff07eef3
Jatin Parmar <jatinparmar980@gmail.com>
2023-11-02 10:22:39 +0530

    Time: 248 ms (46.23%), Space: 10.8 MB (55.61%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> v(nums.size(),1);
+        for(int i=0; i<nums.size(); i++){
+            for(int j=0; j<i; j++){
+                if(nums[i] > nums[j]){
+                    v[i] = max(v[j]+1,v[i]);
+                }
+            }
+        }
+        return *max_element(v.begin(),v.end());
+    }
+};
