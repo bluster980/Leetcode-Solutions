6a9e60ea99a8ea9e11d4d750dccb907a03c0dd57
Jatin Parmar <jatinparmar980@gmail.com>
2023-11-19 22:56:11 +0530

    Time: 161 ms (62.88%), Space: 83.1 MB (68.40%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
@@ -4,10 +4,10 @@ public:
         sort(nums.begin(),nums.end());
         int ans=0,j=0;
         for(int i=1;i<nums.size();i++){
-			if(nums[i]!=nums[i-1]){  //if number is not equal to previous then we need one more step so we increase j
+			if(nums[i]!=nums[i-1]){
                 j++;
             }
-            ans+=j; //count or every number present in array
+            ans+=j;
         }
         return  ans;
     }
