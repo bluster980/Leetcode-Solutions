ba4474f53fa6a331930b049f68aaacb00e9cc3e4
Jatin Parmar <jatinparmar980@gmail.com>
2023-11-22 23:31:27 +0530

    Time: 197 ms (62.60%), Space: 97.8 MB (46.58%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
         int maxKey = 0;
         unordered_map<int, vector<int>> map;
-        for (int r = nums.size() - 1; r >= 0; --r) { // The values from the bottom rows are the starting values of the diagonals.
+        for (int r = nums.size() - 1; r >= 0; --r) {
             for (int c = 0; c < nums[r].size(); ++c) {
                 map[r + c].push_back(nums[r][c]);
                 maxKey = max(maxKey, r + c);
