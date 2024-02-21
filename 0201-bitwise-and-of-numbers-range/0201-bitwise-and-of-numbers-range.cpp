70ae902edf21f743c323dca8b32001378ceec9ec
Jatin Parmar <jatinparmar980@gmail.com>
2024-02-22 00:30:57 +0530

    Time: 4 ms (79.27%), Space: 13.6 MB (8.30%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-        int cnt = 0;
-        while (left != right) {
-            left >>= 1;
-            right >>= 1;
-            cnt++;
+        vector<int> x(32,0), y(32,0);
+        for(int i=0; i<32; i++){
+            if(left & (1 << i)) x[i] = 1;
         }
-        return (left << cnt);
+        for(int i=0; i<32; i++){
+            if(right & (1 << i)) y[i] = 1;
+        }
+        // for(int i=31; i>=0; i--) cout<<x[i];
+        // cout<<endl;
+        // for(int i=31; i>=0; i--) cout<<y[i];
+        // cout<<endl;
+        int ans = 0;
+        for(int i=31; i>=0; i--){
+            int temp = 0;
+            if(y[i] && !x[i]) break;
+            else if(x[i] && y[i]){
+                temp = temp | (1 << i);
+                ans = ans | temp;
+                // cout<<ans<<endl;
+            }
+        }
+        // cout<<endl;
+        return ans;
     }
 };
