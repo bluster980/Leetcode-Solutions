7688374d1cb3dd05ae00fbe81071d72730d22596
Jatin Parmar <jatinparmar980@gmail.com>
2024-02-22 00:31:34 +0530

    Time: 8 ms (53.91%), Space: 13.6 MB (8.30%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -4,14 +4,8 @@ public:
         vector<int> x(32,0), y(32,0);
         for(int i=0; i<32; i++){
             if(left & (1 << i)) x[i] = 1;
-        }
-        for(int i=0; i<32; i++){
             if(right & (1 << i)) y[i] = 1;
         }
-        // for(int i=31; i>=0; i--) cout<<x[i];
-        // cout<<endl;
-        // for(int i=31; i>=0; i--) cout<<y[i];
-        // cout<<endl;
         int ans = 0;
         for(int i=31; i>=0; i--){
             int temp = 0;
@@ -19,10 +13,8 @@ public:
             else if(x[i] && y[i]){
                 temp = temp | (1 << i);
                 ans = ans | temp;
-                // cout<<ans<<endl;
             }
         }
-        // cout<<endl;
         return ans;
     }
 };
