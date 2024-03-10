785a46c98ed1acc47ec4939484dfbad3e4e714b7
Jatin Parmar <jatinparmar980@gmail.com>
2024-03-10 13:05:20 +0530

    Time: 175 ms (34.23%), Space: 362.6 MB (7.80%) - LeetHub

--- 0398-random-pick-index/0398-random-pick-index.cpp
+++ 0398-random-pick-index/0398-random-pick-index.cpp
@@ -1,26 +1,16 @@
 class Solution {
-private:
-    std::unordered_map<int, std::vector<int>> indices;
-
 public:
-    Solution(std::vector<int>& nums) {
-        for (int i = 0; i < nums.size(); ++i) {
-            indices[nums[i]].push_back(i);
+    unordered_map<int, vector<int>> mp;
+    Solution(vector<int>& nums) {
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]].push_back(i);
         }
     }
-
     int pick(int target) {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-
-        if (indices.find(target) != indices.end()) {
-            std::vector<int>& targetIndices = indices[target];
-            std::uniform_int_distribution<int> dist(0, targetIndices.size() - 1);
-            int randomIndex = dist(gen);
-            return targetIndices[randomIndex];
-        }
-
-        return -1;
+        vector<int> temp = mp[target];
+        int ran = (rand() % temp.size());
+        // cout<<ran<<endl;
+        return temp[ran];
     }
 };
 
