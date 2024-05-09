bb5caf7b25480c7266a360a7b2b1db514ace31bd
Jatin Parmar <jatinparmar980@gmail.com>
2024-05-09 16:56:23 +0530

    Time: 153 ms (81.32%), Space: 107 MB (89.28%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& h, int k) {
-        priority_queue<long long> pq;
-        for(int i=0; i<h.size(); i++){
-            pq.push(h[i]);
-        }
-        long long x = 0, ans = 0;
-        while(!pq.empty() && k--){
-            ans += max(0*1ll,(pq.top()-x)); x++;
-            pq.pop();
+        sort(h.begin(),h.end());
+        long long x = 0, ans = 0, i=h.size()-1;
+        while(i>=0 && k--){
+            ans += max(0*1ll,(h[i]-x)); x++;
+            i--;
         }
         return ans;
     }
