b0fdeb29c177afea640909ab206b1c61565539ea
Jatin Parmar <jatinparmar980@gmail.com>
2024-06-08 16:56:59 +0530

    Time: 660 ms (17.26%), Space: 478 MB (14.30%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -5,32 +5,27 @@ public:
         for(int i=0; i<d.size(); i++){
             mp[d[i]]++;
         }
-        vector<string> temp;
-        int i=0, j = 0;
-        while(i < s.size()){
-            while(s[i] != ' '){
-                i++;
-            }
-            string dumy = string(s.begin()+j, s.begin()+i);
-            // cout<<dumy<<endl;
-            temp.push_back(dumy);
-            i++; j = i;
-        }
-        string ans;
-        for(int i=0; i<temp.size(); i++){
-            int k = 0;
-            while(k <= temp[i].size()){
-                if(mp.find(temp[i].substr(0, k)) != mp.end()){
-                    ans += temp[i].substr(0, k);
-                    ans.push_back(' ');
-                    break;
+        string temp; string ans;
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == ' '){
+                int j = 0;
+                while(j < temp.size()){
+                    if(mp.count(temp.substr(0,j))){
+                        ans += temp.substr(0,j);
+                        ans += " "; break;
+                    }
+                    j++;
                 }
-                k++;
+                if(j == temp.size()){
+                    ans += temp;
+                    ans += " ";
+                }
+                temp = "";
             }
-            if(k > temp[i].size()){
-                ans += temp[i];
-                ans.push_back(' ');
+            else{
+                temp.push_back(s[i]);
             }
+            
         }
         ans.pop_back();
         return ans;
