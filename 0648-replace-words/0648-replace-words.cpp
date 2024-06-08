6993ce533a3001b1630817886d2a0b2e684245a5
Jatin Parmar <jatinparmar980@gmail.com>
2024-06-08 16:23:35 +0530

    Time: 706 ms (13.83%), Space: 480.3 MB (10.64%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
-    string replaceWords(vector<string>& dictionary, string sentence) {
-        unordered_map<string,int> st;
-        for(int i=0; i<dictionary.size(); i++){
-            st[dictionary[i]]++;
+    string replaceWords(vector<string>& d, string s) {
+        unordered_map<string,int> mp; s.push_back(' ');
+        for(int i=0; i<d.size(); i++){
+            mp[d[i]]++;
         }
-        string word="",ans="";
-        sentence += ' ';
-        for(auto i: sentence){
-            if(i==' '){
-                if(ans.size()>0) ans += ' ';
-                string temp = "";
-                for(auto j: word){
-                    temp += j;
-                    if(st.count(temp)) {
-                        break;
-                    }
+        vector<string> temp;
+        int i=0, j = 0;
+        while(i < s.size()){
+            while(s[i] != ' '){
+                i++;
+            }
+            string dumy = string(s.begin()+j, s.begin()+i);
+            // cout<<dumy<<endl;
+            temp.push_back(dumy);
+            i++; j = i;
+        }
+        string ans;
+        for(int i=0; i<temp.size(); i++){
+            int k = 0;
+            while(k <= temp[i].size()){
+                if(mp.find(temp[i].substr(0, k)) != mp.end()){
+                    ans += temp[i].substr(0, k);
+                    ans.push_back(' ');
+                    break;
                 }
-                ans += temp;
-                word = "";
-            } else word += i;
+                k++;
+            }
+            if(k > temp[i].size()){
+                ans += temp[i];
+                ans.push_back(' ');
+            }
         }
+        ans.pop_back();
         return ans;
     }
 };
