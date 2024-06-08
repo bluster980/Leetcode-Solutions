8fc6d522dc36cd9ce86d8fe2bbb8b9e3842155bd
Jatin Parmar <jatinparmar980@gmail.com>
2024-06-08 15:21:25 +0530

    Time: 1082 ms (8.04%), Space: 478.7 MB (13.71%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    string replaceWords(vector<string>& dictionary, string sentence) {
-        unordered_set<string> st(dictionary.begin(),dictionary.end());
-        string word="",ans="";
-        sentence += ' ';
-        for(auto i: sentence){
-            if(i==' '){
-                if(ans.size()>0) ans += ' ';
-                string temp = "";
-                for(auto j: word){
-                    temp += j;
-                    if(st.count(temp)) {
-                        break;
-                    }
-                }
-                ans += temp;
-                word = "";
-            } else word += i;
+    string replaceWords(vector<string>& d, string s) {
+        map<string,int> mp; s.push_back(' ');
+        for(int i=0; i<d.size(); i++){
+            mp[d[i]]++;
         }
+        int j=0; string ans; 
+        for(int i=0; i<s.size(); i++){
+            string temp; bool flag = false;
+            while(s[i] != ' '){
+                temp = string(s.begin()+j , s.begin()+i);
+                if(mp.find(temp) != mp.end()){
+                    ans += temp; flag = true;
+                    ans.push_back(' ');
+                    while(s[i] != ' ') i++;
+                    continue;
+                }
+                i++;
+                
+            }
+            if(!flag) {temp.push_back(s[i-1]); ans += temp; ans.push_back(' ');}
+            j = i+1;
+        }
+        ans.pop_back();
         return ans;
     }
 };
